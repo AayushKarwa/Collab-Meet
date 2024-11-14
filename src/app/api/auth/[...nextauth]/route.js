9db7763cb83250925fb21c1c6765ca0e60fb6e6a
Ayush karwa <ayushkarwa18@gmail.com>
2024-11-14 11:04:21 +0530

    deployment

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,63 +1,64 @@
 import dbConnect from "@/lib/dbConnect";
-import {UserModel} from "@/model/user";
-import NextAuth from "next-auth"
-import GithubProvider from "next-auth/providers/github"
+import { UserModel } from "@/model/user";
+import NextAuth from "next-auth";
+import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
 
 export const authOptions = {
-  // Configure one or more authentication providers
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
     }),
     GoogleProvider({
-        clientId: process.env.GOOGLE_CLIENT_ID,
-        clientSecret: process.env.GOOGLE_CLIENT_SECRET
-      })
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
   ],
-  callbacks:{
-    async jwt({token,user,account}){
-
-        if(user){
-            token.id = user.id;
-        }
-        if(account){
-            token.accessToken= account.access_token;
-        }
-
-        return token;
+  callbacks: {
+    async jwt({ token, user, account }) {
+      if (user) {
+        // Assign dbUser._id to token.id if available
+        token.id = user.id; 
+      }
+      if (account) {
+        token.accessToken = account.access_token;
+      }
+      return token;
     },
     async session({ session, token }) {
-         session.user.id= token.id;
-        return session
-      },
-
-      async signIn({user,profile}) {
-        await dbConnect();
-        let dbUser = await UserModel.findOne({email: user.email})
-
-        //if user not found create a new user
-        if(!dbUser){
-            dbUser = await UserModel.create({
-                name:profile.name,
-                email:profile.email,
-                profilePicture:profile.picture,
-                isVerified:profile.email_verified ? true :false
-            })
-        }
-         user.id = dbUser._id.toString();
-         return true;
+      if (token) {
+        session.user.id = token.id;
       }
-  },
-  session:{
-    strategy: 'jwt',
-    maxAge : 90 * 24 * 60 * 60
-  },
-  pages:{
-    signIn: '/user-auth',
-  }
-}
+      return session;
+    },
+    async signIn({ user, profile }) {
+      await dbConnect();
+      let dbUser = await UserModel.findOne({ email: user.email });
 
-const handle = NextAuth(authOptions)
-export {handle as POST , handle as GET};
+      // If user not found, create a new user
+      if (!dbUser) {
+        dbUser = await UserModel.create({
+          name: profile.name,
+          email: profile.email,
+          profilePicture: profile.picture,
+          isVerified: profile.email_verified || false,
+        });
+      }
+
+      // Set dbUser._id to user.id to be used in jwt callback
+      user.id = dbUser._id.toString();
+      return true;
+    },
+  },
+  session: {
+    strategy: "jwt",
+    maxAge: 90 * 24 * 60 * 60,
+  },
+  pages: {
+    signIn: "/user-auth",
+  },
+};
+
+const handle = NextAuth(authOptions);
+export { handle as POST, handle as GET };
